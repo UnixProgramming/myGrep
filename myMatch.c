e35f90e006d94c1f86a3803573987c4e99e1cd59
mingu sin <alsrn556@naver.com>
2016-12-03 15:14:05 +0900

    add 'n_parsing function'

--- myMatch.c
+++ myMatch.c
@@ -5,6 +5,21 @@ _Bool optionSet = false; //false : normal  true : all character
 
 //mingu
 void n_parsing(int linecnt, char* line, const char* patt, char* filename, _Bool showFilename){
+	regex_t state;
+    int status;
+    int linecount=linecnt;
+
+    if(regcomp(&state, patt, 0)){
+        perror("pattern parsing error");
+        exit(1);
+    }
+
+    status = regexec(&state, line,0,NULL,0);
+    if(!status)
+        if(showFilename)
+            printf("<%s>%d : %s\n",filename,linecount,line);
+        else
+            printf("%d : %s\n",linecount,line);
 }
 
 //mingu
